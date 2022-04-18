bb453ce0114024fcaf664a2b3dfde25c5abee25b
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-04-18 09:13:35 +0100

    first commit

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -6,9 +6,9 @@ def call(Map config[:]) {
             checkout scmurl
         }
  
-        stage('Build Image') {
+        /*stage('Build Image') {
                     sh 'docker build -t ${config.imageName}'
-        }
+        }*/
         stage('Integration Test') {
 		     steps {
                     sh ' docker-compose -f ${config.dockerCompose} up '
