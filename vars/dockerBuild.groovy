1ba3c71f3fca2bf0d8424019b65cb55294268590
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-04-24 14:48:04 +0100

    first commit

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,11 @@
+def call (Map config)
+{
+    node
+        {
+          stage('Docker build')
+                {  
+                    sh "docker build -t teeeeeest -f  ${config.dockerfileLocation}"
+                }
+         }
+    
+}
---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,15 @@
+def call (Map config)
+{
+    node
+        {
+           stage("gitCheckout") {
+            checkout([$class: 'GitSCM', 
+            branches: [[name: 'refs/heads/main']], 
+            userRemoteConfigs: [[
+                url:"${config.scmurl}"]]
+
+        ])
+    }  
+   
+    
+}
