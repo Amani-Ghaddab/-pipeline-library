e17d16abd542e626ac98644fe75dd9565aa44fe7
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-04-25 08:47:56 +0100

    first commit

--- vars/dockerComposeUp.groovy
+++ vars/dockerComposeUp.groovy
@@ -4,7 +4,7 @@ def call (Map config)
         {
           stage('Docker compose up')
                 {  
-                    sh "${config.dockerComposLocation} up "
+                    sh "Docker-compose up "
                 }
          }
     
