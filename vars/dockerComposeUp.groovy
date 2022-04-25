7e74b7a5526f400e1b02ab53f37c44fc818ecf21
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-04-25 09:27:07 +0100

    first commit

--- vars/dockerComposeUp.groovy
+++ vars/dockerComposeUp.groovy
@@ -4,8 +4,7 @@ def call (Map config)
         {
           stage('Docker compose up')
                 {  
-                    sh"docker stop c934588bb7326960e8575b1df60eff2877cbc6db3378f11c76eaced064aefea7"
-                    sh "docker rm c934588bb7326960e8575b1df60eff2877cbc6db3378f11c76eaced064aefea7"
+                   
                     sh "${config.dockerComposLocation} up "
                 }
          }
