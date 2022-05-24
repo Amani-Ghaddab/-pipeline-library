38ea751f7093ba31a46bd70480f03f3c857572f3
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-05-24 15:54:40 +0100

    new one

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -8,6 +8,23 @@ def call (Map config)
             userRemoteConfigs: [[
                 url:"${config.scmurl}"]]
         ])
-        }  
-     }
+        }
+        stage ('move all file')
+
+            {    
+
+                 sh "ls -la ${pwd()}"
+
+                 sh "ls -la ${pwd()}/source"
+
+                 sh "ls -la ${pwd()}/source/DevOpsProject/"
+
+                 sh "cd /var/jenkins_home/workspace/ | mkdir aoso"
+
+                 sh "cp -r ${pwd()}/source/DevOpsProject /var/jenkins_home/workspace/aoso"
+
+                 sh "ls -la /var/jenkins_home/workspace/aoso "
+
+            }  
+    }
 }
