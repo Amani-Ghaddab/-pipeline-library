2cb8e8c2785bf11afae67c752b4713cd7e0d9874
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-05-25 22:06:45 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -13,7 +13,7 @@ def call (Map config)
 
             {    
 
-                sh "if [ ! -d $~${config.destinationFile} ] then mkdir -p ~${config.destinationFile} else echo 'Directory exists' fi"
+                sh "[-d $~${config.destinationFile} ] || mkdir -p ~${config.destinationFile} "
                
               //  sh "cp -r ${pwd()} ~${config.destinationFile}"
                 // | mkdir ${pwd()}/${config.projectName}/DevOps | ${pwd()}/${config.projectName} 
