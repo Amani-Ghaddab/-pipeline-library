064088084f99377987858cb2ae9d0d8d627cb4cd
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-05-25 19:03:03 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -14,8 +14,9 @@ def call (Map config)
             {    
 
                 sh "ls -la ${pwd()}"
-                sh "mkdir ${pwd()}/${config.projectName}/DevOps/backend | mkdir ${pwd()}/${config.projectName}/DevOps | ${pwd()}/${config.projectName} "
-                //|  
+                sh "cd .."
+                sh "mkdir ${config.destinationFile}"
+                // | mkdir ${pwd()}/${config.projectName}/DevOps | ${pwd()}/${config.projectName} 
                 //sh "mkdir /var/jenkins_home/workspace/aoso/ | mkdir /var/jenkins_home/workspace/aoso/DevOps | mkdir /var/jenkins_home/workspace/aoso/DevOps/backend "
                  sh "cp -r ${pwd()}/Source/DevOpsProject ${pwd()}/${config.projectName}/"
                 
