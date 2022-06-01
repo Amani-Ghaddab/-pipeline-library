cf6fda27002ece10d1019151a1761e6be292b327
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-01 10:17:34 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -24,7 +24,7 @@ def call (Map config)
             }  
         stage('Docker build')
                 {  
-                    sh "/usr/local/bin/docker-compose -f ~${config.destinationFile}/docker-compose-back.yml up -d"
+                    sh "docker-compose -f ~${config.destinationFile}/docker-compose-back.yml up -d"
                 }
     }
 }
