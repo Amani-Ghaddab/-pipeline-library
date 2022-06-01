dc44603df64397627c5a8a11109da2ce5d8402b0
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-01 10:29:04 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -20,7 +20,7 @@ def call (Map config)
         //         sh " ls ~${config.destinationFile}"
         //         sh " ls ~${config.destinationFile}/nginx"
         //       //  sh " ls ~${config.destinationFile}/AosoFront"
-        //     }  
+            }  
         // stage('Docker build')
         //         {  
         //             sh "docker-compose -f ~${config.destinationFile}/docker-compose-back.yml up -d"
