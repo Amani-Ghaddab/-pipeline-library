b3c05d7925f6cdeec2d6ae70a7fe7aee0398656f
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-04 18:33:05 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -15,6 +15,7 @@ def call (Map config)
                 sh " mkdir -p ${config.destinationFile} "
                 sh " cp -r Source/${config.projectName} ${config.destinationFile}"
                 sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFile}"
+                sh " ls -la ${config.destinationFile} "
             }  
         stage('Docker build')
                 {  
