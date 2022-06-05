93bd74cd9f57deda031b90a754a23810c9361e4f
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-05 15:06:25 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -30,25 +30,25 @@ def call (Map config)
         }
         }
         }
-        stage("gitCheckout") {
-            checkout([$class: 'GitSCM', 
-            branches: [[name: 'refs/heads/main']], 
-            userRemoteConfigs: [[
-                url:"${config.scmurl}"]]
-        ])
-        }
-        stage ('create the project directory')
-            {   
-                sh " ls -la ${pwd()}"
-                sh " mkdir -p ${config.destinationFile} "
-                sh " cp -r Source/${config.projectName} ${config.destinationFile}"
-                sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFile}"
-                sh " ls -la ${config.destinationFile} "
-            }  
-        stage('Docker build')
-                {  
-                    sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
-                }
+        // stage("gitCheckout") {
+        //     checkout([$class: 'GitSCM', 
+        //     branches: [[name: 'refs/heads/main']], 
+        //     userRemoteConfigs: [[
+        //         url:"${config.scmurl}"]]
+        // ])
+        // }
+        // stage ('create the project directory')
+        //     {   
+        //         sh " ls -la ${pwd()}"
+        //         sh " mkdir -p ${config.destinationFile} "
+        //         sh " cp -r Source/${config.projectName} ${config.destinationFile}"
+        //         sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFile}"
+        //         sh " ls -la ${config.destinationFile} "
+        //     }  
+        // stage('Docker build')
+        //         {  
+        //             sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
+        //         }
     }
 
 }
