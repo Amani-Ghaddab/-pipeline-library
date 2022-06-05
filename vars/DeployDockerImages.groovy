b3cfb089f2404b09932b5b3787e51157f22cd0f7
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-05 15:04:09 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -2,7 +2,35 @@ def call (Map config)
 {
     node
         {
-           stage("gitCheckout") {
+        stage('Sonarqube analysis ') {
+
+        environment { 
+
+            //def scannerHome = tool 'SonarScanner 4.10.0'
+
+            MSBUILD_SQ_SCANNER_HOME = tool name: 'sonarscanner', type: 'hudson.plugins.sonar.MsBuildSQRunnerInstallation'
+
+                }
+
+        steps {
+
+        withSonarQubeEnv('sonarserver') {
+
+            sh 'dotnet restore'
+
+            sh ("""dotnet ${MSBUILD_SQ_SCANNER_HOME}/SonarScanner.MSBuild.dll begin /k:'app_key'""")
+
+            sh "dotnet build app.sln"
+
+            sh "dotnet ${MSBUILD_SQ_SCANNER_HOME}/SonarScanner.MSBuild.dll end"
+// dotnet tool install --global dotnet-sonarscanner
+// dotnet sonarscanner begin /k:"project-key" /d:sonar.login="<token>"
+// dotnet build <path to solution.sln>
+// dotnet sonarscanner end /d:sonar.login="<token>"
+        }
+        }
+        }
+        stage("gitCheckout") {
             checkout([$class: 'GitSCM', 
             branches: [[name: 'refs/heads/main']], 
             userRemoteConfigs: [[
@@ -22,4 +50,5 @@ def call (Map config)
                     sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
                 }
     }
+
 }
