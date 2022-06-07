1862114284cd953b91b9d4300be10b81b870a369
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-07 15:47:29 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -11,28 +11,28 @@ def call (Map config)
         ])
             
         }
-    //    stage('SonarQube analysis') {
-    //      def scannerHome = tool 'sonarscanner';
-    //      //def MSBUILD_SQ_SCANNER_HOME = tool name: 'sonarscanner', type: 'hudson.plugins.sonar.MsBuildSQRunnerInstallation';
+       stage('SonarQube analysis') {
+         def scannerHome = tool 'sonarscanner';
+         //def MSBUILD_SQ_SCANNER_HOME = tool name: 'sonarscanner', type: 'hudson.plugins.sonar.MsBuildSQRunnerInstallation';
 
-    //          withSonarQubeEnv('SonarQube') {
-    //              dir("Source/${config.projectName}") {
-    //                 sh " ls -la ${pwd()}"
-    //                // sh "dotnet tool install --global dotnet-sonarscanner"
-    //                sh ("""${scannerHome}/bin/sonar-scanner begin -D /k:"AosoDevops" -D /d:sonar.host.url="http://localhost:9000" -D /d:sonar.login="23b5d4c1c1f76c539f1d0019945228a4003d6a51""")
+             withSonarQubeEnv('SonarQube') {
+                 dir("Source/${config.projectName}") {
+                    sh " ls -la ${pwd()}"
+                   // sh "dotnet tool install --global dotnet-sonarscanner"
+                   sh ("""${scannerHome}/bin/sonar-scanner begin -D /k:"AosoDevops" -D /d:sonar.host.url="http://localhost:9000" -D /d:sonar.login="23b5d4c1c1f76c539f1d0019945228a4003d6a51""")
 
-    //                 sh "dotnet build DevOpsProject.csproj"
+                    sh "dotnet build DevOpsProject.csproj"
 
-    //                 sh "${scannerHome}/bin/sonar-scanner end"}
+                    sh "${scannerHome}/bin/sonar-scanner end"}
 
-      stage('SonarQube Analysis') {
-        def scannerHome = tool 'SonarScanner for MSBuild'
-        withSonarQubeEnv('SonarQube') {
-        sh " ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"AosoDevops\" "
-        sh "dotnet build DevOpsProject.csproj"
-        sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"
+    //   stage('SonarQube Analysis') {
+    //     def scannerHome = tool 'SonarScanner for MSBuild'
+    //     withSonarQubeEnv('SonarQube') {
+    //     sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"AosoDevops\" "
+    //     sh "dotnet build DevOpsProject.csproj"
+    //     sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"
     
-      }
+    //   }
     //     stage ('create the project directory')
     //         {   
                
@@ -46,7 +46,7 @@ def call (Map config)
     //                 sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
     //             }
      }
-
+       }
 
         }
 }
