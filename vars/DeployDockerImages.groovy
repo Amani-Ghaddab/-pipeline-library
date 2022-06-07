e9eaf722e6c3c1d19f4a1451e6e300d9ce62d568
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-07 12:59:11 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -13,7 +13,7 @@ def call (Map config)
         }
        stage('SonarQube analysis') {
          def scannerHome = tool 'sonarscanner';
-         def MSBUILD_SQ_SCANNER_HOME = tool name: 'sonarscanner', type: 'hudson.plugins.sonar.MsBuildSQRunnerInstallation';
+         //def MSBUILD_SQ_SCANNER_HOME = tool name: 'sonarscanner', type: 'hudson.plugins.sonar.MsBuildSQRunnerInstallation';
 
              withSonarQubeEnv('SonarQube') {
                  dir("Source/${config.projectName}") {
