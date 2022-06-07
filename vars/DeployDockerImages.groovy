bed030d91da69b49a666d4f31aa1ff8360804a6c
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-07 16:23:48 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -19,7 +19,9 @@ def call (Map config)
                  dir("Source/${config.projectName}") {
                     sh " ls -la ${pwd()}"
                    // sh "dotnet tool install --global dotnet-sonarscanner"
-                    sh "${MSBUILD_SQ_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=AosoDevops -Dsonar.sources=. -Dsonar.host.url=http://localhost:9000 -Dsonar.login=23b5d4c1c1f76c539f1d0019945228a4003d6a51 "
+                    sh ("${MSBUILD_SQ_SCANNER_HOME}SonarScanner.MSBuild.dll begin k:'devopsAoso' /d:sonar.host.url='http://localhost:9000'")
+
+
                 //    sh ("${scannerHome}/bin/sonar-scanner begin -D /k:AosoDevops -D /d:sonar.host.url=http://localhost:9000 -D /d:sonar.login=23b5d4c1c1f76c539f1d0019945228a4003d6a51")
 
                     sh "dotnet build DevOpsProject.csproj"
