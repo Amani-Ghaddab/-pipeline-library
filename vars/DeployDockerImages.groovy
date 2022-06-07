2a01e1afeca6e772ac1923b2d8a6373798877d5c
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-07 10:21:45 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -20,38 +20,34 @@ def call (Map config)
 
     //              }
 
-        // steps {
+        steps {
 
-            // withSonarQubeEnv('SonarQube') {
+            withSonarQubeEnv('SonarQube') {
                 
-            //     dir("Source/${config.projectName}") {
-            //          sh " ls -la ${pwd()}"
-            //         sh 'dotnet restore'
+                dir("Source/${config.projectName}") {
+                    sh " ls -la ${pwd()}"
 
-            //         sh ("""dotnet ${MSBUILD_SQ_SCANNER_HOME}/SonarScanner.MSBuild.dll begin /k:'app_key'""")
+                    sh ("""dotnet sonarscanner begin /k:"devopsAoso" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="8140e1097f7a42cd390c0e807ffe8e62ed5b705a""")
 
-            //         sh "dotnet build app.sln"
+                    sh "dotnet build DevOpsProject.csproj"
 
-            //         sh "dotnet ${MSBUILD_SQ_SCANNER_HOME}/SonarScanner.MSBuild.dll end"}
-// dotnet tool install --global dotnet-sonarscanner
-// dotnet sonarscanner begin /k:"project-key" /d:sonar.login="<token>"
-// dotnet build <path to solution.sln>
-// dotnet sonarscanner end /d:sonar.login="<token>"
-      //  }
+                    sh ("""dotnet sonarscanner end /d:sonar.login="8140e1097f7a42cd390c0e807ffe8e62ed5b705a""")}
+
+       }
        
       
-        stage ('create the project directory')
-            {   
+        // stage ('create the project directory')
+        //     {   
                
-                sh " mkdir -p ${config.destinationFile} "
-                sh " cp -r Source/${config.projectName} ${config.destinationFile}"
-                sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFile}"
-                sh " ls -la ${config.destinationFile} "
-            }  
-        stage('Docker build')
-                {  
-                    sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
-                }
+        //         sh " mkdir -p ${config.destinationFile} "
+        //         sh " cp -r Source/${config.projectName} ${config.destinationFile}"
+        //         sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFile}"
+        //         sh " ls -la ${config.destinationFile} "
+        //     }  
+        // stage('Docker build')
+        //         {  
+        //             sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
+        //         }
     }
 
 }
