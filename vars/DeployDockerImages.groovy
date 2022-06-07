b7679dd07caff90bc31fbbc202bc8093318484cb
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-07 15:50:47 +0100

    first commit

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -19,7 +19,7 @@ def call (Map config)
                  dir("Source/${config.projectName}") {
                     sh " ls -la ${pwd()}"
                    // sh "dotnet tool install --global dotnet-sonarscanner"
-                   sh ("""${scannerHome}/bin/sonar-scanner begin -D /k:AosoDevops -D /d:sonar.host.url=http://localhost:9000 -D /d:sonar.login="23b5d4c1c1f76c539f1d0019945228a4003d6a51""")
+                   sh ("""${scannerHome}/bin/sonar-scanner -D /k:AosoDevops -D /d:sonar.host.url=http://localhost:9000 -D /d:sonar.login="23b5d4c1c1f76c539f1d0019945228a4003d6a51""")
 
                     sh "dotnet build DevOpsProject.csproj"
 
