52cb3f0928e0cbb80ef6e7fba1d69ae641230ba7
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-09 14:01:15 +0100

    first commit

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -5,7 +5,7 @@ def call (Map config)
             stage("gitCheckout") {
                 sh"echo ${config.branche}"
             checkout([$class: 'GitSCM', 
-            branches: [[name: ${config.branche}]], 
+            branches: [[name: "${config.branche}"]], 
             userRemoteConfigs: [[
                 url:"${config.scmurl}"]]
           
