5114d6418cace5ac2b2c660747822ae66f0bc3d5
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-13 13:10:37 +0100

    first commit

--- vars/sonarQubeForAngular.groovy
+++ vars/sonarQubeForAngular.groovy
@@ -1,18 +1,20 @@
 ef call (Map config)
 {
-
     node
         {
         environment {
         scannerHome = tool 'SonarQubeScanner'
-    }
-          stage('quality code analysis') {
+        }
+     
+        stage('quality code analysis') {
               
                 withSonarQubeEnv('sonarqube') { 
-                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=AosoFront -Dsonar.login=23b5d4c1c1f76c539f1d0019945228a4003d6a51"                }
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=AosoFront -Dsonar.login=23b5d4c1c1f76c539f1d0019945228a4003d6a51"      
+                              }
                 // timeout(time: 10, unit: 'MINUTES') {
                 //     waitForQualityGate abortPipeline: true
-                // }
-            }
-          }
+                 }
+        
         }
+        
+}
