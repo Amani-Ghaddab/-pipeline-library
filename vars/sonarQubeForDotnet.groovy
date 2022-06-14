16d03fb8d96bb32877aeae2a7f1c8f4ff9576f75
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-14 11:01:02 +0100

    first commit

--- vars/sonarQubeForDotnet.groovy
+++ vars/sonarQubeForDotnet.groovy
@@ -16,19 +16,17 @@ def call (Map config)
                     //println ${env.SONAR_HOST_URL} 
              }
       }
-     stage("Quality Gate"){
-  timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
-    def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
-    if (qg.status != 'OK') {
-      error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    }
+//      stage("Quality Gate"){
+//   timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+//     def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+//     if (qg.status != 'OK') {
+//       error "Pipeline aborted due to quality gate failure: ${qg.status}"
+//     }
    
-    // stage("Quality gate") {
+    stage("Quality gate") {
            
-    //             waitForQualityGate abortPipeline: true
+                waitForQualityGate abortPipeline: true
             
-    //     }
-}
-}
         }
+}
 }
