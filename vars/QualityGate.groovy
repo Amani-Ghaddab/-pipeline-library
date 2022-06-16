323a3140acdd0d80484406e52f60c78d4e2d06e0
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-16 14:43:44 +0100

    first

--- vars/QualityGate.groovy
+++ vars/QualityGate.groovy
@@ -8,8 +8,19 @@ def call (Map config)
           sleep(10)
           if (qualitygate.status != "OK") {
             waitForQualityGate abortPipeline: true
+            // mail to: "ameni.ghaddabe@gmail.com",
+            // subject: "Test Email",
+            // body: "Test"
+            error "Pipeline aborted due to quality gate failure: ${qualitygate.status}"
           }
+          post{
+        failure{
+            emailext to: "ameni.ghaddabe@gmail.com",
+            subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
+            body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL}"
+        }
           }
+
             
         }
 }
