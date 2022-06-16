8913dc260114cdb0eca7ee8adde5575dc2ec87e0
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-16 15:26:34 +0100

    first

--- vars/QualityGate.groovy
+++ vars/QualityGate.groovy
@@ -11,9 +11,9 @@ def call (Map config)
             // mail to: "ameni.ghaddabe@gmail.com",
             // subject: "Test Email",
             // body: "Test"
-             emailext to: "ameni.ghaddabe@gmail.com",
-            subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
-            body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL}"
+            //  emailext to: "ameni.ghaddabe@gmail.com",
+            // subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
+            // body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL}"
       
             error "Pipeline aborted due to quality gate failure: ${qualitygate.status}"
           }
--- vars/sonarQubeForDotnet.groovy
+++ vars/sonarQubeForDotnet.groovy
@@ -3,13 +3,10 @@ def call (Map config)
 
     node
         {
-    //    stage ("install sonarqube"){
-    //        sh " /usr/local/bin/docker-compose -f Scripts/Back/sonarQube/docker-compose.yml up -d --build "
-                
-    //    }
+        
+    
        stage('quality code analysis') {
-    //withCredentials([string(credentialsId: 'adminsonarqube', variable: 'ADMIN_SONNARQUBE')]) {
-            
+              
          def scannerHome = tool "${config.SonarScannerMSBuildTool}"
              withSonarQubeEnv("${config.SonarQubeEnv}") { 
                  dir("Source/${config.projectName}") {
@@ -17,15 +14,10 @@ def call (Map config)
                     sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"${config.projectName}\" /d:sonar.login=${config.sonarLogin}"
                     sh "dotnet build"
                     sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=${config.sonarLogin}"}
-                    //println ${env.SONAR_HOST_URL} 
+                
              }
       }
-//      stage("Quality Gate"){
-//   timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
-//     def qg = waitForQualityGate(webhookSecretId: 'jenkinsSonar') // Reuse taskId previously collected by withSonarQubeEnv
-//     if (qg.status != 'OK') {
-//       error "Pipeline aborted due to quality gate failure: ${qg.status}"
-//     }
+
    
   
 }
