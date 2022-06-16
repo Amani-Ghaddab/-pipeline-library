7b0986fef3db27f8a49685d469b4ec1b5136baa8
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-16 16:42:19 +0100

    first

--- vars/sonarQubeForAngular.groovy
+++ vars/sonarQubeForAngular.groovy
@@ -3,14 +3,12 @@ def call (Map config)
     node
         {
         def scannerHome = tool 'SonarScanner'
-             withSonarQubeEnv("${config.SonarQubeEnv}") { 
+              withSonarQubeEnv("${config.SonarQubeEnv}") { 
                  dir("Source/${config.projectName}") {
               
                 
-                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=AosoFront -Dsonar.login=${config.sonarLogin}"      
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${config.projectName} -Dsonar.login=${config.sonarLogin}"      
                               }
-                // timeout(time: 10, unit: 'MINUTES') {
-                //     waitForQualityGate abortPipeline: true
                  }
         
         }
