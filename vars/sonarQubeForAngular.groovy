aa80d1a4c64bf69ce5bfe63f07a33ec4b45e294f
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-21 14:42:31 +0100

    first

--- vars/sonarQubeForAngular.groovy
+++ vars/sonarQubeForAngular.groovy
@@ -2,15 +2,16 @@ def call (Map config)
 {
     node
         {
-        def scannerHome = tool 'SonarScanner'
-              withSonarQubeEnv("${config.SonarQubeEnv}") { 
-                 dir("Source/${config.projectName}") {
-              
+           stage('quality code analysis') {
+            def scannerHome = tool 'SonarScanner'
+                withSonarQubeEnv("${config.SonarQubeEnv}") { 
+                    dir("Source/${config.projectName}") {
                 
-                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${config.projectName} -Dsonar.login=${config.sonarLogin}"      
-                              }
-                 }
+                    
+                        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${config.projectName} -Dsonar.login=${config.sonarLogin}"      
+                                }
+                    }
         
         }
         
-}
+}}
