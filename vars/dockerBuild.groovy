a448550adfec7b651448e72c93a4711375d6b923
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-21 15:14:10 +0100

    first

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,9 @@ def call (Map config)
 {
     node
         {
+             when {
+                environment name:'APPROVED_DEPLOY', value: 'yes'
+            }
          stage('Docker build and up')
                 {  
                     sh " ${config.dockerComposeLocation}/docker-compose -f ${config.dockerComposeFileDestination} -p ${config.containerName} up -d --build "
