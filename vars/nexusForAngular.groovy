ef426dd10a08a766ae388369ecf839c395da4ee9
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-24 22:37:58 +0100

    first commit

--- vars/nexusForAngular.groovy
+++ vars/nexusForAngular.groovy
@@ -13,7 +13,7 @@ def call (Map config)
   
     sh 'cd dist/DevOpsFront'
     sh' npm pack'
-    sh"npm config set registry http://localhost:8081/"
+    sh"npm config set registry https://registry.npmjs.com/"
     //sh'npm publish'
     sh" npm login --registry=http://localhost:8081/repository/AosoFront/"
    // | echo admin | echo 123456 | amani.ghaddab@eniso.u-sousse.tn"
