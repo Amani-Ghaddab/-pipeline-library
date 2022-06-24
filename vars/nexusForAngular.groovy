3f2282ee3c487162fb6aa27611316bb482aab16d
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-24 18:04:42 +0100

    first

--- vars/nexusForAngular.groovy
+++ vars/nexusForAngular.groovy
@@ -15,7 +15,7 @@ def call (Map config)
     sh' npm pack'
     
     //sh'npm publish'
-    sh" npm login --registry=http://localhost:8081/repository/AosoFront | echo admin | echo 123456"
+    sh" npm login --registry=http://localhost:8081/repository/AosoFront | echo admin | echo 123456 | ameni.ghaddabe@gmail.com"
   }
   
         
