9e0998c65558169f7f2a7fd71c50df71e8b539ea
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-30 19:45:58 +0100

    first commit

--- vars/nexusForAngular.groovy
+++ vars/nexusForAngular.groovy
@@ -14,7 +14,7 @@ def call (Map config)
     sh 'cd dist/DevOpsFront'
     sh' npm pack'
     //sh'npm publish'
-   sh" npm publish --registry http://localhost:8081/repository/npm-registry/ "
+   sh" npm publish --registry http://localhost:8081/repository/npm-private/ "
   //  sh"npm login -u admin -p 123456 --registry http://localhost:8081/repository/npm-registry/"
   //  sh'npm publish'
   }
