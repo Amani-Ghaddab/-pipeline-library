39d8078059d9f8c1280149d29081b20687220af5
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-30 13:29:05 +0100

    first commit

--- vars/nexusForDotnet.groovy
+++ vars/nexusForDotnet.groovy
@@ -71,11 +71,11 @@ def call (Map config)
    stage('Creating our image'){      
                         //def version = "latest"
                         //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
-                        dir("Scripts/Back/")
-                            {
+                   
+                            
                             dockerImage = docker.build "docker_back/front" + ":latest" 
-                            }
-                    }
+                            
+                    
                         stage('push image in nexus'){      
                                 //def version = "latest"
                                 //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
