bf56b4cc7b7ee19640e4503bce1d4f053ab49f91
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-30 13:06:26 +0100

    first commit

--- vars/nexusForDotnet.groovy
+++ vars/nexusForDotnet.groovy
@@ -23,53 +23,68 @@ def call (Map config)
 //     sh "dotnet nuget push nupkgs/*.nupkg -s http://localhost:8081/repository/AosoBack/ -k f25611e8-6ecc-3587-b95c-6be0ec3248ee"
    
 //         }  
- dir ("/srv/Aoso/DevOps/backend"){
-    environment {
-        imageName = "back"
-        registryCredentials = "nexus"
-        registry = "ec2-13-58-223-172.us-east-2.compute.amazonaws.com:8085/"
-        dockerImage = ''
-    }
+//  dir ("/srv/Aoso/DevOps/backend"){
+//     environment {
+//         imageName = "back"
+//         registryCredentials = "nexus"
+//         registry = "ec2-13-58-223-172.us-east-2.compute.amazonaws.com:8085/"
+//         dockerImage = ''
+//     }
     
     
-    // Building Docker images
-    stage('Building image') {
+//     // Building Docker images
+//     stage('Building image') {
     
-        script {
-          dockerImage = docker.build back
-        }
+//         script {
+//           dockerImage = docker.build back
+//         }
       
-    }
+//     }
 
-    // Uploading Docker images into Nexus Registry
-    stage('Uploading to Nexus') {
+//     // Uploading Docker images into Nexus Registry
+//     stage('Uploading to Nexus') {
     
-         script {
-             docker.withRegistry( 'http://'+registry, registryCredentials ) {
-             dockerImage.push('latest')
-          }
+//          script {
+//              docker.withRegistry( 'http://'+registry, registryCredentials ) {
+//              dockerImage.push('latest')
+//           }
         
-      }
-    }
+//       }
+//     }
     
-    // Stopping Docker containers for cleaner Docker run
-    stage('stop previous containers') {
+//     // Stopping Docker containers for cleaner Docker run
+//     stage('stop previous containers') {
         
-            sh 'docker ps -f name=back -q | xargs --no-run-if-empty docker container stop'
-            sh 'docker container ls -a -fname=back -q | xargs -r docker container rm'
+//             sh 'docker ps -f name=back -q | xargs --no-run-if-empty docker container stop'
+//             sh 'docker container ls -a -fname=back -q | xargs -r docker container rm'
          
-       }
+//        }
       
-    stage('Docker Run') {
+//     stage('Docker Run') {
      
-         script {
-                sh 'docker run -d -p 80:80 --rm --name back ' + registry + imageName
-            }
+//          script {
+//                 sh 'docker run -d -p 80:80 --rm --name back ' + registry + imageName
+//             }
          
-      }    
-    }
+//       }    
+//     }
+   stage('Creating our image'){      
+                        //def version = "latest"
+                        //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
+                        dir("Scripts/Back/Dockerfile}")
+                            {
+                            dockerImage = docker.build "dockerForBack/front" + ":latest" 
+                            }
+                    }
+                        stage('push image in nexus'){      
+                                //def version = "latest"
+                                //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
+                                docker.withRegistry( 'http://localhost:8082/repository/dockerForBack', 'dockerForBack' ) { 
+                                    dockerImage.push() 
+                                } 
+                             }
+        }
 }
 
-}
      
     
