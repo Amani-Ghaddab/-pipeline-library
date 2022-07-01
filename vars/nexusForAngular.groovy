7a63e2007328eff6f011feeda30321fee88fa7f5
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-01 16:10:12 +0100

    up

--- vars/nexusForAngular.groovy
+++ vars/nexusForAngular.groovy
@@ -10,9 +10,7 @@ def call (Map config)
  
   }
   stage('Creating our image'){      
-                        //def version = "latest"
-                        //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
-                     dir ("/srv/Aoso/DevOps/frontend"){
+                        dir ("/srv/Aoso/DevOps/frontend"){
                         dockerImage = docker.build "docker_front/aoso_front" + ":latest"
                     }
    stage('push image in nexus'){      
@@ -20,18 +18,7 @@ def call (Map config)
             dockerImage.push() 
                                 } 
                              }
-}
-  // stage('Pack') {
-  
-  //   sh 'cd dist/DevOpsFront'
-  //   sh' npm pack'
-  //   sh'npm publish'
-  //  //sh" npm publish --registry http://localhost:8081/repository/npm-private/ "
-  // //  sh"npm login -u admin -p 123456 --registry http://localhost:8081/repository/npm-registry/"
-  // //  sh'npm publish'
-  // }
-  
-        
+}       
 }
      
     
