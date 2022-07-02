ccf30a59ff1f41e730059ef7553ced8962604ebd
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-02 18:55:41 +0100

    first

--- vars/nexusForDotnet.groovy
+++ vars/nexusForDotnet.groovy
@@ -72,9 +72,9 @@ def call (Map config)
                         //def version = "latest"
                         //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
                     // dir ("/srv/Aoso/DevOps/backend"){
-                     dir ("/srv/Aoso/DevOps/backend"){
+                     sh'cd Scripts/Back'
                         dockerImage = docker.build "docker_back/aoso_back" + ":latest" --no-cache
-                    }
+                    
    stage('push image in nexus'){      
              docker.withRegistry( 'http://localhost:8082/repository/dockerForBack', 'nexus' ) { 
             dockerImage.push() 
