00b1dc7f586ac79f3d797de43a088156b32cb262
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-02 21:26:29 +0100

    first

--- vars/nexusForDotnet.groovy
+++ vars/nexusForDotnet.groovy
@@ -1,38 +1,21 @@
 def call (Map config)
 {
-//    node{
-//   dir("cd Scripts/Back") {  
-//        sh"ls -a"
-//                         //def version = "latest"
-//                         //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
-//                     // dir ("/srv/Aoso/DevOps/backend"){
-//                      sh'cd Scripts/Back'
-//                         dockerImage = docker.build "docker_back/aoso_back" + ":latest" --no-cache
+    node{
+   stage('Creating our image'){      
+                        //def version = "latest"
+                        //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
+                    dir ("/srv/Aoso/DevOps/backend"){
                     
-//    stage('push image in nexus'){      
-//              docker.withRegistry( 'http://localhost:8082/repository/dockerForBack', 'nexus' ) { 
-//             dockerImage.push() 
-//                                 } 
-//                              }
-//         }
-// }
-
-   node{
-  dir("Scripts/Back") {
-      sh"ls -a"
-
-  stage('Creating our image'){      
-                        dir ("/Scripts/Back"){
-                         dockerImage = docker.build "docker_back/aoso_back" + ":latest"
-                    }}
+                        dockerImage = docker.build "docker_back/aoso_back" + ":latest" --no-cache
+                    
    stage('push image in nexus'){      
-             docker.withRegistry( 'http://localhost:8083/repository/dockerForFront', 'nexus' ) { 
+             docker.withRegistry( 'http://localhost:8082/repository/dockerForBack', 'nexus' ) { 
             dockerImage.push() 
                                 } 
                              }
-}       
+        }
 }
+
      
-    
 }
-     
+}
