bee231741b8a1ec7173462b0f9ae0c5530a28be5
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-02 19:54:43 +0100

    first

--- vars/nexusForDotnet.groovy
+++ vars/nexusForDotnet.groovy
@@ -18,17 +18,12 @@ def call (Map config)
 // }
 
    node{
-  dir("Source/AosoFront") {
+  dir("Scripts/Back") {
       sh"ls -a"
 
-  stage('Build') {
-  
-    sh 'ng build'
- 
-  }
   stage('Creating our image'){      
-                        dir ("/srv/Aoso/DevOps/frontend"){
-                        dockerImage = docker.build "docker_front/aoso_front" + ":latest"
+                        dir ("/Scripts/Back"){
+                         dockerImage = docker.build "docker_back/aoso_back" + ":latest"
                     }
    stage('push image in nexus'){      
              docker.withRegistry( 'http://localhost:8083/repository/dockerForFront', 'nexus' ) { 
