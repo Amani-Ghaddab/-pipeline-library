74939afd6dad84c7a0fb7dab66839533fbdd5525
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-03 21:39:13 +0100

    first

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -5,7 +5,7 @@ def call (Map config)
       
         stage ('create the local project directory')
             {   
-                sh " sudo chmod -R 700 /srv"
+                sh " sudo chmod -R a+w /srv"
                 sh " mkdir -p ${config.destinationFolder} "
                 sh " cp -r Source/${config.projectName} ${config.destinationFolder}"
                 sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFolder}"
