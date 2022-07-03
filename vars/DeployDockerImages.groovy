933f8fbee7e36faebe221ae03294948ccbf53f96
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-03 21:21:56 +0100

    first

--- vars/DeployDockerImages.groovy
+++ vars/DeployDockerImages.groovy
@@ -5,8 +5,8 @@ def call (Map config)
       
         stage ('create the local project directory')
             {   
-                sh "ssh amani@192.168.69.21 ' mkdir -p /srv/Aoso/DevOps/backend'"
-                //sh " mkdir -p ${config.destinationFolder} "
+                sh "chmod -R 777"
+                sh " mkdir -p ${config.destinationFolder} "
                 sh " cp -r Source/${config.projectName} ${config.destinationFolder}"
                 sh " cp -r ${config.dockerComposeFileLocation} ${config.dockerFileLocation} ${config.nginxLocation} ${config.destinationFolder}"
                 sh " ls -la ${config.destinationFolder} "
