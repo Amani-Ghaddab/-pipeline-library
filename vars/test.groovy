e1c1a6e0dca50f2d641fef3e4385168d547bb266
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-05 16:47:02 +0100

    first

---
+++ vars/test.groovy
@@ -0,0 +1,23 @@
+def call (Map config)
+{
+    node
+        {
+           stage('Approval Step'){
+        
+                
+                //----------------send an approval prompt-------------
+                script {
+                   env.APPROVED_BUILD = input message: 'User input required',
+                   parameters: [choice(name: 'Build?', choices: 'no\nyes', description: 'Choose "yes" if you want to Build this image')]
+                       }
+                //-----------------end approval prompt------------
+            }
+           }
+        // def doesJavaRock = input(message: 'Do you like Java?', ok: 'Yes', 
+        //                 parameters: [booleanParam(defaultValue: true, 
+        //                 description: 'If you like Java, just push the button',name: 'Yes?')])
+
+        //     echo "Java rocks?:" + doesJavaRock
+        }  
+     
+    
