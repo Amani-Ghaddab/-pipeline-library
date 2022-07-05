f2cf59a8e3d223eff8463f4061d39cbd9d0fde62
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-05 21:00:07 +0100

    first

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -15,7 +15,39 @@ def call (Map config)
         //                 description: 'If you like Java, just push the button',name: 'Yes?')])
 
         //     echo "Java rocks?:" + doesJavaRock
-        }  
+        }
+        stage('test') {
+    
+              sh 'whoami'
+              sh'touch amani'
+              sh 'ls'
+            
+            }
+    try {
+        stage('Test') {
+            sh 'echo "Fail!"; exit 1'
+        }
+        
+    } catch (e) {
+        echo 'This will run only if failed'
+
+        // Since we're catching the exception in order to report on it,
+        // we need to re-throw it, to ensure that the build is marked as failed
+        throw e
+    } finally {
+        def currentResult = currentBuild.result ?: 'SUCCESS'
+        if (currentResult == 'UNSTABLE') {
+            echo 'This will run only if the run was marked as unstable'
+        }
+
+        def previousResult = currentBuild.getPreviousBuild()?.result
+        if (previousResult != null && previousResult != currentResult) {
+            echo 'This will run only if the state of the Pipeline has changed'
+            echo 'For example, if the Pipeline was previously failing but is now successful'
+        }
+
+        echo 'This will always run'
+    }  
      }
     
 }
