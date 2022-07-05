91c2db8078622d02d3cb8c8c0c4c9d73d6c01e0c
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-05 22:11:56 +0100

    first

--- vars/QualityGate.groovy
+++ vars/QualityGate.groovy
@@ -3,21 +3,20 @@ def call (Map config)
 
     node
         {
+         
            stage("Quality gate") {
+             if( env.test){
+               sh 'echo hello'
+             }
           def qualitygate = waitForQualityGate()
          
           if (qualitygate.status != "OK") {
             waitForQualityGate abortPipeline: true
-            // mail to: "ameni.ghaddabe@gmail.com",
-            // subject: "Test Email",
-            // body: "Test"
-            //  emailext to: "ameni.ghaddabe@gmail.com",
-            // subject: "jenkins build:${currentBuild.currentResult}: ${env.JOB_NAME}",
-            // body: "${currentBuild.currentResult}: Job ${env.JOB_NAME}\nMore Info can be found here: ${env.BUILD_URL}"
+                
       
             error "Pipeline aborted due to quality gate failure: ${qualitygate.status}"
           }
-          sh"echo mregel"
+         
            }
 
             
--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,6 +2,7 @@ def call (Map config)
 {
     node
         {
+             env.test= true
             stage("gitCheckout") {
                 sh"echo ${config.branche}"
             checkout([$class: 'GitSCM', 
@@ -16,38 +17,7 @@ def call (Map config)
 
         //     echo "Java rocks?:" + doesJavaRock
         }
-        stage('test') {
-    
-              sh 'whoami'
-              sh'touch amani'
-              sh 'ls'
-            
-            }
-    try {
-        stage('Test') {
-            sh 'echo "Fail!"; exit 1'
-        }
-        
-    } catch (e) {
-        echo 'This will run only if failed'
-
-        // Since we're catching the exception in order to report on it,
-        // we need to re-throw it, to ensure that the build is marked as failed
-        throw e
-    } finally {
-        def currentResult = currentBuild.result ?: 'SUCCESS'
-        if (currentResult == 'UNSTABLE') {
-            echo 'This will run only if the run was marked as unstable'
-        }
-
-        def previousResult = currentBuild.getPreviousBuild()?.result
-        if (previousResult != null && previousResult != currentResult) {
-            echo 'This will run only if the state of the Pipeline has changed'
-            echo 'For example, if the Pipeline was previously failing but is now successful'
-        }
-
-        echo 'This will always run'
-    }  
+          
      }
     
 }
