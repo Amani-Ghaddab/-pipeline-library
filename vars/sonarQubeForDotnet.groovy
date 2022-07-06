6a47fcab4099a3f264103a1cdf02652d2598f7d9
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-07-06 09:22:50 +0100

    first

--- vars/sonarQubeForDotnet.groovy
+++ vars/sonarQubeForDotnet.groovy
@@ -6,15 +6,16 @@ def call (Map config)
     
        stage('quality code analysis') {
             when { env.test=='false'
-               sh 'echo hello' + env.test
+               
             }
-             def scannerHome = tool "${config.SonarScannerMSBuildTool}"
-             withSonarQubeEnv("${config.SonarQubeEnv}") { 
-                 dir("Source/${config.projectName}") {
-                    sh " ls -la ${pwd()}"
-                    sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"${config.projectName}\" /d:sonar.login=${config.sonarLogin}"
-                    sh "dotnet build"
-                    sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=${config.sonarLogin}"}
+            sh 'echo hello' + env.test
+            //  def scannerHome = tool "${config.SonarScannerMSBuildTool}"
+            //  withSonarQubeEnv("${config.SonarQubeEnv}") { 
+            //      dir("Source/${config.projectName}") {
+            //         sh " ls -la ${pwd()}"
+            //         sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"${config.projectName}\" /d:sonar.login=${config.sonarLogin}"
+            //         sh "dotnet build"
+            //         sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=${config.sonarLogin}"}
                 
              }}
       }
